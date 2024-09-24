1d22c4cd1f3a60a250f29eca530ced8387f220f5
janaelewis <jlewi1193635@mail.wcccd.edu>
2024-09-23 23:25:33 -0400

    Add files via upload

---
+++ Chapter 1/project02/project01-02.js
@@ -0,0 +1,24 @@
+/*  JavaScript 7th Edition
+    Chapter 1
+    Hands-On Project 1-2
+
+    Author: Janae Lewis
+    Date:   9/9/24
+
+    Filename: project01-02.js
+*/
+<script>
+//define variables for service name and service speed
+var service1Name = "Basic"
+   var service2Name = "Express"
+   var service3Name = "Extreme"
+   var service4Name = "Ultimate"
+   var service1Speed = 5
+   var service2Speed = 25
+   var service3Speed = 50
+   var service4Speed = 150
+   var service1Speed = "5"
+service1name = “Basic” service2Name = “Express”, service3Name = “Extreme”
+service4Name = “Ultimate”, service1Speed = “0 Mbps”, service2Speed = “100
+Mbps”, service3Speed = “500 Mbps”, and service4Speed = “1 Gig”
+</script>
